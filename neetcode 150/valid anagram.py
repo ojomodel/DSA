acc0f6db0df1c388de5a6c2fa44e7b40b1b8c959
ojomodel <seunomodelee@gmail.com>
2026-01-15 02:09:15 -0600

    Initial commit

---
+++ neetcode 150/valid anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashset = set() # {r, a, c, e}
+        list_s = list(s) #[r, a, c, e, c, a, r]
+        list_t = list(t) #[c, a, r, r, a, c, e]
+        ashset = set()
+        for x in list_s:
+            if x in hashset:
+                ashset.add(x)
+                hashset.remove(x)
+            else:
+                hashset.add(x)
+        alone = next(iter(hashset))
+        if alone in list_t:
+            return True
+        else:
+            return False
+# s = "racecar"
+# t = "carrace"
+s = "jar"
+t = "jam"
+print(Solution().isAnagram(s, t))
